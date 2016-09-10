19875def106aa9d5011765f1ac54a6878c7bdc9b
Jeffrey Hsieh <jhsie007@ucr.edu>
2016-09-09 21:27:00 -0700

    typoe fix??

--- animation.js
+++ animation.js
@@ -1,10 +1,11 @@
 
-$('a[href^="#"').on('click', function(autumn){
+$('a[href^="#"]').on('click', function(autumn){
   var path = $(this.getAttribute('href'));
 
   if(path.length){
+    //Prevent from opening new browser
     autumn.preventDefault();
-    $('html, body').stop().animate({scrollTop: path.offset().top}, 1000);
+    $('html, body').stop().animate({scrollTop: path.offset().top}, 750);
   }
 
 });
