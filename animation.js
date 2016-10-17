908f7638b8b7f03c74c51b56d114cbd8568eb7c1
Jeffrey Hsieh <jhsie007@ucr.edu>
2016-10-17 11:33:59 -0700

    heart

--- animation.js
+++ animation.js
@@ -11,11 +11,16 @@ $(document).ready(function(){
 var modal = document.getElementById('re_modal');
 var modal_image = document.getElementById('re_modal_image');
 var re_bot = document.getElementById("re_bot");
-
+var re_heart = document.getElementById("re_heart");
 
 re_bot.onclick = function() {
   modal.style.display = "block";
 }
+
+re_heart.onclick = function() {
+  modal.style.display = "block";
+}
+
 
 window.onclick = function(event) {
   if(event.target == modal || event.target == re_modal_image) {
