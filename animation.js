5cf294c474fc037d10bc2656b29e5a4a1560449f
Jeffrey Hsieh <jhsie007@ucr.edu>
2016-10-17 11:04:23 -0700

    revert

--- animation.js
+++ animation.js
@@ -11,11 +11,9 @@ $(document).ready(function(){
 var modal = document.getElementById('re_modal');
 var re_bot = document.getElementById("re_bot");
 
-var rem = document.getElementByID("re_rem");
 
 re_bot.onclick = function() {
   this.modal.style.display = "block";
-  this.modal.style.right-margin = "20px";
 }
 
 window.onclick = function(event) {
