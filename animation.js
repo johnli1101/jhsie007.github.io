0c4ff83df57fb03aea50c86686febffd853b3d89
Jeffrey Hsieh <jhsie007@ucr.edu>
2016-11-16 21:03:16 -0800

    Update: project page

--- animation.js
+++ animation.js
@@ -20,3 +20,23 @@ function hide_modal(modal_id) {
   modal_identity.style.display = "none";
 }
 
+function touch() {
+}
+
+function menu() {
+  var menu = document.getElementById("Re_Menu");
+  var world = document.getElementById("Re_World");
+  var offset = world.scrollTop;
+  if(offset != 0) {
+    menu.style.top = 0;
+    menu.style.marginTop = "0";
+  }
+  else {
+    menu.style.top = "";
+    menu.style.marginTop = "";
+  }
+}
+
+$(document).ready(function() {
+
+})
